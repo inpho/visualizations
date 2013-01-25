30d47ddf5d4e5e30123ba9d43d6c8df655b79d4a
Samuel Waggoner <samuel.waggoner@gmail.com>
2013-01-25 13:33:45 -0500

    adding graph projects.

---
+++ projects/graph/weighted/weighted-graph.js
@@ -0,0 +1,67 @@
+var width = 960, height = 500;
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+  .charge(-120)
+  .linkDistance(30)
+  .size([width, height]);
+
+var svg = d3.select("#chart").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+d3.json("miserables.json", function(error, graph) {
+  force
+    .nodes(graph.nodes)
+    .links(graph.links)
+    .start();
+
+  var link = svg.selectAll(".link")
+    .data(graph.links)
+    .enter().append("line")
+    .attr("class", "link")
+    .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = svg.selectAll(".node")
+    .data(graph.nodes)
+    .enter().append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  node.append("circle")
+    .attr("r", 5)
+    .style("fill", function(d) { return color(d.group); })
+    .call(force.drag);
+
+  node.append("title")
+    .text(function(d) { return d.name; });
+
+  node.append("text")
+    .attr("dx", ".3em")
+    .style("text-anchor", "start")
+    .text( function(d) { return d.name; });
+  
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
+
+  /*
+  function update() {
+    link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+  }
+
+  for (var i = 0; i < 1000; i++) {
+    update();
+  }*/
+  
+  
+});
