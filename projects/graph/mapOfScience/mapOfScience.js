5ee9edd45e8e2464e53535b4537f1feb7682b028
Samuel Waggoner <samuel.waggoner@gmail.com>
2013-02-08 19:46:18 -0500

    Scale slider and filter slider added. Scale Slider working... Filter slider not so much. TODO: add zoom slider + panning.

--- projects/graph/mapOfScience/mapOfScience.js
+++ projects/graph/mapOfScience/mapOfScience.js
@@ -6,8 +6,7 @@ var height = 400;
 var topMargin = 200;
 var leftMargin = 300;
 
-var xScale = (window.innerWidth - leftMargin) / width;
-var yScale = (window.innerHeight - topMargin) / height;
+var scale = (window.innerWidth - leftMargin) / width;
 
 var color = {
   "Blue":d3.rgb("#0000FF"),
@@ -27,36 +26,103 @@ var color = {
 
 var force = d3.layout.force()
   .charge(-120)
-  .size([width*xScale, height*yScale]);
+  .size([width*scale, height*scale]);
 
-var svg = d3.select("#chart").append("svg")
-  .attr("width", width * xScale)
-  .attr("height", height * yScale);
+var chart = d3.select("#chart")
+
+
+var scaleDiv = chart.append("div").attr("id","#scaleDiv");
+
+var scaleSlider = scaleDiv
+  .append("input")
+  .attr("type", "range")
+  .attr("name", "scale")
+  .attr("id", "scaleSlider")
+  .attr("min", "1")
+  .attr("max", "4")
+  .attr("step", ".1")
+  .attr("value", scale);
+scaleDiv.append("text").text("Scale");
+
+var filterDiv = chart.append("div").attr("id", "#filterDiv");
+
+
+var filterSlider = filterDiv.append("input")
+  .attr("type", "range")
+  .attr("name", "scale")
+  .attr("id", "filterSlider")
+  .attr("min", "0")
+  .attr("max", "500")
+  .attr("step", "5")
+  .attr("value", scale);
+filterDiv.append("text").text("Filter Weights");
+
+filterSlider.on("change", function(d) {
+  console.log(this.value);
+  
+});
+
+
+
+var svg = chart.append("svg")
+  .attr("width", width * scale)
+  .attr("height", height * scale);
+
+var force;
+var data;
 
 d3.json("mapOfScienceData.json", function(error, graph) {
-  force
+  data = graph;
+  update(graph);
+});
+ 
+ 
+function update(graph) {
+  force = force
     .nodes(graph.nodes)
     .links(graph.links)
-    .start();
+    .start()
+    .stop();
+
+  svg.attr("width", width * scale)
+    .attr("height", height * scale);
   
   var link = svg.selectAll(".link")
-    .data(graph.links)
-    .enter().append("line")
+    .data(graph.links);
+
+  var linkEnter = link.enter()
+    .append("line")
     .attr("class", "link")
-    .attr("x1", function(d) { return d.source.x * xScale; })
-    .attr("y1", function(d) { return d.source.y * yScale; })
-    .attr("x2", function(d) { return d.target.x * xScale; })
-    .attr("y2", function(d) { return d.target.y * yScale; })
     .style("stroke-width", function(d) { return Math.sqrt(d.weight)/ 4; })
     .style("stroke", function(d) { if (d.source.color === d.target.color) {
       return color[d.source.color];} else { return "#ccc"; } });
 
+  var linkUpdate = link
+    .attr("x1", function(d) { return d.source.x * scale; })
+    .attr("y1", function(d) { return d.source.y * scale; })
+    .attr("x2", function(d) { return d.target.x * scale; })
+    .attr("y2", function(d) { return d.target.y * scale; });
+  
+  var linkExit = link.exit()
+    .remove();
+  
+  
   var node = svg.selectAll(".node")
-    .data(graph.nodes)
-    .enter().append("g")
-    .attr("class", "node")
-    .attr("transform", function(d) { return "translate(" + (d.x * xScale) + "," + (d.y * yScale) + ")"; });
+    .data(graph.nodes);
 
+  var nodeEnter = node.enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + (d.x * scale) + "," + (d.y * scale) + ")"; });
+
+  var nodeUpdate = node
+    .attr("transform", function(d) { return "translate(" + (d.x * scale) + "," + (d.y * scale) + ")"; });
+
+  var nodeExit = node.exit()
+    .transition()
+    .duration(500)
+    .remove();
+  
   node.filter( function(d) { return d.group === 1; })
     .append("text")
     .attr("dx", "1em")
@@ -64,35 +130,23 @@ d3.json("mapOfScienceData.json", function(error, graph) {
     .style("stroke", function(d) { return color[d.color].darker(2); })
     .style("text-anchor", "start")
     .text( function(d) { return d.name; });
-
+  
   node.append("circle")
     .attr("r", function(d) { return d.xfact; })
-    .style("fill", function(d) { return color[d.color]; })
-    .call(force.drag);
-
+    .style("fill", function(d) { return color[d.color]; });
+  
   node.append("title")
     .text(function(d) { return d.name; });
 
 
-  var redraw = function() {
-    xScale = (window.innerWidth - leftMargin) / width;
-    yScale = (window.innerHeight - topMargin) / height;
-    
-    svg.attr("width", width * xScale)
-      .attr("height", height * yScale);
-    
-    node.attr("transform", function(d) { return "translate(" + (d.x * xScale) + "," + (d.y * yScale) + ")"; });
-
-    link
-      .attr("x1", function(d) { return d.source.x * xScale; })
-      .attr("y1", function(d) { return d.source.y * yScale; })
-      .attr("x2", function(d) { return d.target.x * xScale; })
-      .attr("y2", function(d) { return d.target.y * yScale; });
-  };
+  scaleSlider.on("change", function(event) {
+    scale = this.value;
+    update(graph);
+  });
   
-  window.onresize = function(event) {
-    redraw();
-  }
+  filterSlider.on("change", function(event) {
+    // Requires more thought...
+    update(node.filter(function(d) { return d.value >= this.value; }));
+  });
+}
 
-  force.stop();  
-});
